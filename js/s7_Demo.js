1f9d7f82e6e02a1304143b997a1156314319afc5
Alex Marcy <alexmarcy@gmail.com>
2015-05-27 21:52:16 -0500

    update s7demo

--- js/s7_Demo.js
+++ js/s7_Demo.js
@@ -1,3 +1,20 @@
 $(function(){
-	alert("Demo Loaded");
+	$.ajax({
+    		url:'http://192.168.0.1/DataLogs/MyDataLog.csv',
+    		type:'get',
+    		dataType:'text',
+    		success:function(data){
+    			var trendData = [];
+    			var result = $.csv.toObjects(data);
+    			var options="";
+
+			for(var i in result){
+				trendData[i]=[result[i].Record,result[i].Record]
+				
+			}
+			flot1 = $.plot($('#trend'), trendData, options);
+        			
+			
+    		}
+	})
 });
