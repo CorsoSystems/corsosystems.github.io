cd0dec908ef81c0d0df7fd20bbb41b6816c2d32b
Alex Marcy <alexmarcy@gmail.com>
2015-05-29 18:15:01 -0500

    s7 demo

--- js/s7_Demo.js
+++ js/s7_Demo.js
@@ -23,8 +23,12 @@ $(function(){
 			}
 
 
+		var d1 = [];
+		for (var i = 0; i < 14; i += 0.5) {
+			d1.push([i, Math.sin(i)]);
+		}
 			
-			$.plot("#trend",trendData);
+			$.plot("#trend",d1);
     		}
 	})
 });
