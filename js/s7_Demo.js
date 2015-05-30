35fe3e04c92c508ef058553e9392d9bf15a09c6a
Alex Marcy <alexmarcy@gmail.com>
2015-05-29 21:02:28 -0500

    s7 demo

--- js/s7_Demo.js
+++ js/s7_Demo.js
@@ -16,7 +16,7 @@ $(function(){
     			
 			for(var i in result){
 				trendData.push([result[i].Record, i])
-				alert(moment(result[i].Date + " "+ result[i]["UTC Time"],"M/DD/YYYY HH:MM:SS"));
+				alert(moment(result[i].Date + " "+ result[i]["UTC Time"]));
 				alert(result[i].Date + " "+ result[i]["UTC Time"],"M/DD/YYYY HH:MM:SS");
 			}
 			
