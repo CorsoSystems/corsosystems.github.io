8a53ac0f6be59643b791d311ba856b946b935607
Alex Marcy <alexmarcy@gmail.com>
2015-06-15 14:07:58 -0500

    s7 demo

--- js/s7_Demo.js
+++ js/s7_Demo.js
@@ -11,29 +11,6 @@ $(function(){
     $('#startDatepicker').val(moment(startDate).format("MM/DD/YYYY h:mm:ss A"));
     $('#endDatepicker').val(moment(date).format("MM/DD/YYYY h:mm:ss A"));
 
-http://192.168.0.1/Portal/Portal.mwsl?PriNav=FileBrowser&Path=/DataLogs/
-
-	$.ajax({
-    		url:'http://192.168.0.1/DataLogs/MyDataLog.csv',
-    		type:'get',
-    		dataType:'text',
-    		success:function(data){
-                                 var result = $.csv.toObjects(data);
-    		           var pump1Data = [];
-                                 var pump2Data = [];
-    			
-    			
-			for(var i in result){
-			     pump1Data.push([Date.parse(result[i].Date + " "+ result[i]["UTC Time"]),result[i].Pump1_On])
-                                      pump2Data.push([Date.parse(result[i].Date + " "+ result[i]["UTC Time"]),result[i].Pump2_On])
-			}
-			
-			$.plot("#trend",[pump1Data,pump2Data],{
-				xaxis: { mode: "time" }
-			});
-    		}
-	})
-
     $.ajax({
         url:'http://192.168.0.1/Portal/Portal.mwsl?PriNav=FileBrowser&Path=/DataLogs/',
         type:'get',
@@ -47,24 +24,31 @@ http://192.168.0.1/Portal/Portal.mwsl?PriNav=FileBrowser&Path=/DataLogs/
 
             $('#fileTable').html($(response).find('#fileBrowserTable').html());
 
-            $('#fileTable').find('.systemTime .fbTime').each(function(index){
-                if($(this).text().length>7){
-                    timestamp = Date.parse(moment($(this).text(),"hh:mm:ss a MM/DD/YYYY"));
-                    //if(timestamp>=start && timestamp<= end){
-                        //alert($(this).parent().parent().closest('.fileBrowserName .hiddenOnSmall').html());
-                        fileName = $(this).closest('.fileBrowserName').find('span').find('a').text();
-                        
-                    //}
-                }
-            });
-
             $('#fileTable').find('tr').each(function(index){
                 if($(this).find('.fbChanged .systemTime .fbTime').text().length>7){
                     timestamp = Date.parse(moment($(this).text(),"hh:mm:ss a MM/DD/YYYY"));
                     //if(timestamp>=start && timestamp<= end){
-                        alert($(this).find('.fileBrowserName .hiddenOnSmall a').text());
-                        fileName = $(this).closest('.fileBrowserName').find('span').find('a').text();
+                        fileName = $(this).find('.fileBrowserName .hiddenOnSmall a').text();
+
+                        $.ajax({
+                            url:'http://192.168.0.1/DataLogs/'+fileName,
+                            type:'get',
+                            dataType:'text',
+                            success:function(data){
+                                var result = $.csv.toObjects(data);
+                                var pump1Data = [];
+                                var pump2Data = [];
                         
+                                for(var i in result){
+                                    pump1Data.push([Date.parse(result[i].Date + " "+ result[i]["UTC Time"]),result[i].Pump1_On])
+                                    pump2Data.push([Date.parse(result[i].Date + " "+ result[i]["UTC Time"]),result[i].Pump2_On])
+                                }
+            
+                               $.plot("#trend",[pump1Data,pump2Data],{
+                                    xaxis: { mode: "time" }
+                                });
+                            }
+                        })
                     //}
                 }
             });
