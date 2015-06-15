3ffab7cc1f8d1668846ede55d32df29511c4a19a
Alex Marcy <alexmarcy@gmail.com>
2015-06-15 11:39:30 -0500

    s7

--- js/s7_Demo.js
+++ js/s7_Demo.js
@@ -49,7 +49,7 @@ http://192.168.0.1/Portal/Portal.mwsl?PriNav=FileBrowser&Path=/DataLogs/
                 if($(this).text().length>7){
                     timestamp = Date.parse(moment($(this).text(),"hh:mm:ss a MM/DD/YYYY"));
                     //if(timestamp>=start && timestamp<= end){
-                        fileName = $(this).closest('.fileBrowserName').find('a').text();
+                        fileName = $(this).closest('.fileBrowserName').find('span').find('a').text();
                         alert(fileName);
                     //}
                 }
